30d0db81e92732dcc77d7afc9cb9e6546cf7cd48
Daniel Szalay <sza.dan.uj@gmail.com>
2024-12-12 23:37:24 +0100

    create badge active switch ui component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">Hello</div>
+  <div id="app">
+    <BadgeActiveSwitch />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import BadgeActiveSwitch from "./components/ui/BadgeActiveSwitch.vue";
 
 export default Vue.extend({
   name: "App",
-  components: {},
+  components: { BadgeActiveSwitch },
 });
 </script>
---
+++ src/components/ui/BadgeActiveSwitch.vue
@@ -0,0 +1,68 @@
+<template>
+  <label class="switch">
+    <input type="checkbox" />
+    <span class="slider"></span>
+  </label>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "badge-active-switch",
+});
+</script>
+
+<style scoped>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 40.4px;
+  height: 20.05px;
+}
+
+.switch input {
+  opacity: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #f9f9f9;
+  outline: 0.59px solid #afc6bd;
+  transition: 0.3s;
+  border-radius: 29.49px;
+  box-shadow: inset 0px 0px 2px rgba(0, 0, 0, 0.3);
+}
+
+.slider::before {
+  position: absolute;
+  content: "";
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  background-color: white;
+  outline: 0.58978px solid #f2ebdb;
+  transition: all 0.3s ease-in-out;
+}
+
+input:checked + .slider::before {
+  outline: 0.68px solid #3b755f;
+}
+
+input:checked + .slider {
+  background-color: #3b755f;
+  outline: 0.68px solid #b0b0b0;
+}
+
+input:checked + .slider::before {
+  transform: translateX(22px);
+}
+
+.switch:hover .slider::before {
+  box-shadow: 0 0 0 5px rgba(59, 117, 95, 0.3);
+}
+</style>
