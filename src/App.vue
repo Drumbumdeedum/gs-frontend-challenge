86bc7a894ba7761223196e2b02c89b52b967760b
Daniel Szalay <sza.dan.uj@gmail.com>
2024-12-13 00:01:37 +0100

    add form radio button basics

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <BadgeActiveSwitch />
+    <FormRadioButton :backgroundColor="'green'" />
+    <FormRadioButton :backgroundColor="'blue'" />
+    <FormRadioButton :backgroundColor="'red'" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import BadgeActiveSwitch from "./components/ui/BadgeActiveSwitch.vue";
+import FormRadioButton from "./components/form/FormRadioButton.vue";
 
 export default Vue.extend({
   name: "App",
-  components: { BadgeActiveSwitch },
+  components: { BadgeActiveSwitch, FormRadioButton },
 });
 </script>
---
+++ src/components/form/FormRadioButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <label>
+    <input type="radio" name="badge-theme" :style="{ backgroundColor }" />
+  </label>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "form-radio-button",
+  props: {
+    /* value: {
+      type: Boolean,
+      required: true,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    }, */
+    backgroundColor: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+input[type="radio"] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  margin: 0;
+  padding: 0;
+  outline: none;
+  cursor: pointer;
+  width: 16px;
+  height: 16px;
+  box-shadow: 0px 4px 4px 0px #00000003;
+  border: 1.5px solid transparent;
+  transition: border 0.3s ease-in-out;
+}
+
+input[type="radio"]:hover {
+  opacity: 0.8;
+}
+
+input[type="radio"]:checked {
+  border: 1.5px solid #b0b0b0;
+}
+</style>
