25e58e6546906a0507ff963fc8d637d351cbdc85
Daniel Szalay <sza.dan.uj@gmail.com>
2024-12-13 16:45:33 +0100

    add WidgetItem testcases

--- tests/unit/WidgetItem.spec.ts
+++ tests/unit/WidgetItem.spec.ts
@@ -1,9 +1,10 @@
-// tests/WidgetItemCard.spec.ts
 import { mount } from "@vue/test-utils";
-import WidgetItem from "@/components/WidgetItem.vue"; // Adjust the import path as needed
+import WidgetItem from "@/components/WidgetItem.vue";
+
+const mockChangeWidgetLinkedStatus = jest.fn();
 
 describe("WidgetItem", () => {
-  it("should render the component", () => {
+  it("Renders a widget item with props: black, 10, carbon, offsets", async () => {
     const wrapper = mount(WidgetItem, {
       propsData: {
         widget: {
@@ -13,10 +14,93 @@ describe("WidgetItem", () => {
           selectedColor: "black",
           amount: 10,
           type: "carbon",
-          action: "reduce carbon footprint",
+          action: "offsets",
+        },
+        mocks: {
+          $store: {
+            dispatch: mockChangeWidgetLinkedStatus,
+          },
         },
       },
     });
+
     expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find("h2").text()).toBe("10kgs of carbon");
+    expect(wrapper.find("h3").text()).toBe("This product offsets");
+
+    const vm = wrapper.vm as Vue & {
+      headerText: string;
+      subheaderText: string;
+    };
+    expect(vm.headerText).toBe("10kgs of carbon");
+    expect(vm.subheaderText).toBe("This product offsets");
+  });
+});
+
+describe("WidgetItem", () => {
+  it("Renders a widget item with props: green, 2, trees, plants", async () => {
+    const wrapper = mount(WidgetItem, {
+      propsData: {
+        widget: {
+          id: 1,
+          linked: false,
+          active: false,
+          selectedColor: "green",
+          amount: 2,
+          type: "trees",
+          action: "plants",
+        },
+        mocks: {
+          $store: {
+            dispatch: mockChangeWidgetLinkedStatus,
+          },
+        },
+      },
+    });
+
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find("h2").text()).toBe("2 trees");
+    expect(wrapper.find("h3").text()).toBe("This product plants");
+
+    const vm = wrapper.vm as Vue & {
+      headerText: string;
+      subheaderText: string;
+    };
+    expect(vm.headerText).toBe("2 trees");
+    expect(vm.subheaderText).toBe("This product plants");
+  });
+});
+
+describe("WidgetItem", () => {
+  it("Renders a widget item with props: beige, 300, plastic, collects", async () => {
+    const wrapper = mount(WidgetItem, {
+      propsData: {
+        widget: {
+          id: 1,
+          linked: false,
+          active: false,
+          selectedColor: "green",
+          amount: 300,
+          type: "plastic",
+          action: "collects",
+        },
+        mocks: {
+          $store: {
+            dispatch: mockChangeWidgetLinkedStatus,
+          },
+        },
+      },
+    });
+
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find("h2").text()).toBe("300 plastic bottles");
+    expect(wrapper.find("h3").text()).toBe("This product collects");
+
+    const vm = wrapper.vm as Vue & {
+      headerText: string;
+      subheaderText: string;
+    };
+    expect(vm.headerText).toBe("300 plastic bottles");
+    expect(vm.subheaderText).toBe("This product collects");
   });
 });
