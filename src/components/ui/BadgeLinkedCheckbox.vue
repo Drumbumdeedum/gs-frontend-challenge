6fe4397698f531d1ea923c8e418eea0210f44827
Daniel Szalay <sza.dan.uj@gmail.com>
2024-12-14 12:16:43 +0100

    fix small :for issue, remove old css

--- src/components/ui/BadgeLinkedCheckbox.vue
+++ src/components/ui/BadgeLinkedCheckbox.vue
@@ -9,7 +9,7 @@
       :value="value"
       :checked="value"
     />
-    <label :for="id">
+    <label :for="`badge-linked-checkbox-${id}`">
       <svg
         class="checkmark"
         xmlns="http://www.w3.org/2000/svg"
@@ -124,66 +124,4 @@ export default Vue.extend({
     }
   }
 }
-
-/* .checkbox {
-  display: inline-flex;
-  align-items: center;
-  cursor: pointer;
-  user-select: none;
-}
-
-.checkbox input {
-  position: absolute;
-  opacity: 0;
-}
-
-.checkbox input:checked + .checkbox-box {
-  background-color: var(--primary-color);
-  border-color: var(--primary-color);
-  color: #ffffff;
-}
-
-.checkbox input:focus-visible + .checkbox-box {
-  box-shadow: 0px 0px 0px 2px rgba(76, 175, 80, 0.8);
-}
-
-.checkbox .checkbox-box {
-  width: 14px;
-  height: 14px;
-  border: 2px solid #323232;
-  color: transparent;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: background-color 0.3s, border-color 0.3s;
-  position: relative;
-}
-
-.checkbox .checkbox-box .checkmark {
-  width: 14px;
-  height: 14px;
-  z-index: 3;
-}
-
-.checkbox::before {
-  opacity: 0;
-  z-index: 2;
-  content: "";
-  position: absolute;
-  width: 36px;
-  height: 36px;
-  background-color: #afc6bd;
-  border-radius: 50%;
-  transform: translateX(50%) translateY(-50%);
-  top: 44%;
-  right: 50%;
-  transition: all 0.3s ease;
-}
-
-@media (hover: hover) {
-  .checkbox:hover::before {
-    opacity: 50%;
-  }
-} */
 </style>
